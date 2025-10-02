1d23e3be0207ac3dc18195a84edb814caf313853
entityJY <jomathya07@gmail.com>
2025-10-01 21:50:00 -0700

    add supabase integration?

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
+import supabase from "../supabaseClient";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-      <h1 className="text-4xl font-bold text-blue-600 mb-6">
-        🚀 Welcome to My First Next.js + Tailwind Page!
-      </h1>
-      <p className="text-lg text-gray-700 mb-6">
-        Edit this text in <code>pages/index.js</code> to make it yours.
-      </p>
-      <button className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">
-        Click Me!
-      </button>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
+            <h1 className="text-4xl font-bold text-blue-600 mb-6">
+                🚀 Welcome to My First Next.js + Tailwind Page!
+            </h1>
+            <p className="text-lg text-gray-700 mb-6">
+                Edit this text in <code>pages/index.js</code> to make it yours.
+                I have now done that.
+            </p>
+            <button className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">
+                Click Me! Or not.
+            </button>
+            <p className="text-lg text-gray-700 mb-6">
+                Hi, this is a website created for the codebox boot camp. I&apos;m Joshua Y, a fun fact about me is that I&apos;ve played the piano for 12 years
+            </p>
+            <input name="information" type="text" id="info" class="searchField"/>
+            <p/>
+            <button onClick={test}>Submit information</button>
+        </main>
+    );
+}
+
+async function test(){
+    var searchField = document.getElementById('info').value
+    console.log(searchField)
+    const { error } = await supabase
+  .from('information')
+  .insert({ id: 1, string: searchField })
 }
